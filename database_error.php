c6f9497a6d04b066487c2762366bd5c4cf8d5076
e-novator <mavuso.bathandwa365@gmail.com>
2022-10-03 15:57:14 +0200

    php product manager application

---
+++ database_error.php
@@ -0,0 +1,25 @@
+<!DOCTYPE html>
+<html>
+
+<!-- the head section -->
+<head>
+    <title>My Guitar Shop</title>
+    <link rel="stylesheet" href="main.css" />
+</head>
+
+<!-- the body section -->
+<body>
+    <header><h1>My Guitar Shop</h1></header>
+
+    <main>
+        <h1>Database Error</h1>
+        <p>There was an error connecting to the database.</p>
+        <p>Error message: <?php echo $error_message; ?></p>
+        <p>&nbsp;</p>
+    </main>
+
+    <footer>
+        <p>&copy; <?php echo date("Y"); ?> <b>e-novator.</b></p>
+    </footer>
+</body>
+</html>
